ffbaefc6db46ebb4a2097261baa5de7118e33968
R cqls <rdrouilh@gmail.com>
2024-05-17 19:38:04 +0200

    add rand.vam

--- R/model.R
+++ R/model.R
@@ -1,6 +1,5 @@
 ## formula <- Time & Type ~ (ARAInf(0.6)| Weibull(1.0,3.0))
 
-#' @export
 vam <- function(form, data = data.frame(), datacov = data.frame()) {
     jl_code <- paste0("@vam(",paste(as.character(form)[-1], collapse=" ~ "), ")")
     vam <- list(
@@ -13,9 +12,10 @@ vam <- function(form, data = data.frame(), datacov = data.frame()) {
     vam
 }
 
-#' @export
+
+rand.vam <- function(vam, n = 30) simulate.vam(vam, n)
+
 simulate.vam <- function(vam, n = 30) {
     toR(jl[[rand]](vam$model, jlvalue(as.integer(n))))
     ## or toR(jlcall("rand", vam$model, as_jlvalue(as.integer(n))))
 }
-
