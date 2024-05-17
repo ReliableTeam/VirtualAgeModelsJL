479d22468595af0eadecde3a20405c4eb9aadd7e
R cqls <rdrouilh@gmail.com>
2024-05-17 21:03:51 +0200

    add mle

--- R/model.R
+++ R/model.R
@@ -12,10 +12,6 @@ vam <- function(form, data = data.frame(), datacov = data.frame()) {
     vam
 }
 
+params <- function(obj, ...) UseMethod("params")
 
-rand.vam <- function(vam, n = 30) simulate.vam(vam, n)
-
-simulate.vam <- function(vam, n = 30) {
-    toR(jl[[rand]](vam$model, jlvalue(as.integer(n))))
-    ## or toR(jlcall("rand", vam$model, as_jlvalue(as.integer(n))))
-}
+params.vam <- function(vam) jl[[params]](vam$model)
