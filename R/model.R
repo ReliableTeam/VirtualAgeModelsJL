42b6c6432218a1544c3b0f55a11b07ef10697f0b
R cqls <rdrouilh@gmail.com>
2024-05-21 07:55:15 +0200

    restore  jlusing VirtualAgeModels

--- R/model.R
+++ R/model.R
@@ -1,12 +1,13 @@
 ## formula <- Time & Type ~ (ARAInf(0.6)| Weibull(1.0,3.0))
 
 vam <- function(form, data = data.frame(), datacov = data.frame()) {
-    jlcode <- paste0("@vam(",paste(as.character(form)[-1], collapse=" ~ "), ")")
+    jlcode <- paste0("@vam(", deparse(rexpr2jlexpr(form)), ")")
     vam <- new.env() # could add later mle when needed
     vam$jl = jlcode
     vam$model = jl(as.name(jlcode))
     vam$data = jlvalue(data)
     vam$datacov = jlvalue(datacov)
+    jl[["data!"]](vam$model, vam$data, vam$datacov)
     class(vam) <- "vam"
     vam
 }
@@ -16,3 +17,9 @@ params <- function(obj, ...) UseMethod("params")
 
 params.vam <- function(vam) jl[[params]](vam$model)
 "params<-.vam" <- function(vam, pars) jl[["params!"]](vam$model, jlvalue(pars))
+
+model.frame.vam <- function(vam, datacov=TRUE) {
+    list(
+        data = jl[[data]](vam$model)),
+        datacov =  
+}
--- R/zzz.R
+++ R/zzz.R
@@ -4,12 +4,14 @@
     } else {
         stop("jl4R needs to be installed! Copy-paste in some bash terminal: /bin/bash -c \"$(curl -fsSL https://raw.githubusercontent.com/rcqls/jl4R/HEAD/inst/install.sh)\" to install it.")
     }
-    # if(R(jlpkgisinstalled(VirtualAgeModels))) {
-    #     jlusing("VirtualAgeModels")
-    #     ## cat("VirtualAgeModels.jl installed...\n")
-    # } else {
-    #     ##jlpkgadd(VirtualAgeModels)
-    #     ## jlpkgadd(url="https://github.com/ReliableTeam/VirtualAgeModels.jl")
-    #     stop("Julia package VirtualAgeModels.jl needs to be installed...")
-    # }
+    if(R(jlpkgisinstalled(VirtualAgeModels))) {
+        jlusing("VirtualAgeModels")
+        ## cat("VirtualAgeModels.jl installed...\n")
+    } else {
+        cat("Julia package VirtualAgeModels.jl is needed...\n")
+        jlpkgadd(VirtualAgeModels)
+        cat("Julia package VirtualAgeModels.jl installed\n")
+        ## jlpkgadd(url="https://github.com/ReliableTeam/VirtualAgeModels.jl")
+        ## stop("Julia package VirtualAgeModels.jl needs to be installed...")
+    }
 }
