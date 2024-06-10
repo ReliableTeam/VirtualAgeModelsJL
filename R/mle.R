5d2b50e07617c223c86f2a38a721bc729bde5cd6
R cqls <rdrouilh@gmail.com>
2024-06-10 04:36:33 +0200

    fix methods after reworking on jk4R

--- R/mle.R
+++ R/mle.R
@@ -2,9 +2,9 @@ mle <- function(obj, ...) UseMethod("mle")
 
 mle.vam <- function(vam, theta=NULL, data = data.frame(), datacov= data.frame()) {
     mle <- if (is.null(theta)) {
-            jl[[mle]](vam$model, jlvalue(data), jlvalue(datacov))
+            jl(mle)(vam$model, data, datacov)
     } else {
-            jl[[mle]](vam$model, jlvalue(theta), jlvalue(data), jlvalue(datacov))
+            jl(mle)(vam$model, theta, data, datacov)
     }
     vam$mle <- mle
     params(vam)
